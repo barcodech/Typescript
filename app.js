7630668ed14819d5f59755cd0a87de0771223ed1
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-05 19:02:30 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+var character = 'Jack';
+var age = 25;
+var barcode = false;
+barcode = true;
+age = 10;
+character = 'Adam';
+var circle = (diameter) => {
+    return diameter * Math.PI;
+};
+console.log(circle(7.5));
---
+++ app.ts
@@ -0,0 +1,13 @@
+var character = 'Jack';
+var age = 25;
+var barcode = false;
+
+barcode = true;
+age = 10;
+character = 'Adam';
+
+var circle = (diameter: number) => {
+    return diameter * Math.PI;
+};
+
+console.log(circle(7.5));
