c8bcd6d5da2e32bba42d3d9b9e8f759a13b6f63e
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-05 19:52:32 +0700

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,16 +1,18 @@
 "use strict";
-const anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} buy ${this.details} for ${this.amount}`;
+    }
 }
-console.log(anchor.href);
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+const invoice1 = new Invoice('Adam', 'shirts', 250);
+const invoice2 = new Invoice('Jack', 'shoes', 300);
+let Invoices = [];
+Invoices.push(invoice1);
+Invoices.push(invoice2);
+invoice1.client = 'Matin';
+console.log(Invoices);
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,26 @@
-const anchor = document.querySelector('a')!;
-if(anchor) {
-  console.log(anchor.href);
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string,d: string,a: number){
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} buy ${this.details} for ${this.amount}`;
+  }
 }
-console.log(anchor.href);
- 
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
- 
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
- 
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
- 
-  console.log(
-    type.value,
-    tofrom.value,
-    details.value,
-    amount.valueAsNumber
-  );
-});
+
+const invoice1 = new Invoice('Adam','shirts',250);
+const invoice2 = new Invoice('Jack','shoes',300);
+
+let Invoices: Invoice[] = [];
+Invoices.push(invoice1);
+Invoices.push(invoice2);
+
+invoice1.client = 'Matin';
+
+console.log(Invoices);
