c07febe44d888f55a640e8d9342451cc1dca53cd
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-05 19:57:40 +0700

    Add files via upload

---
+++ src/classes/Invoice.ts
@@ -0,0 +1,12 @@
+export class Invoice {
+  constructor(
+  readonly client: string,
+  private details: string,
+  public amount: number
+
+  ){}
+
+  format() {
+    return `${this.client} buy ${this.details} for ${this.amount}`;
+  }
+}
