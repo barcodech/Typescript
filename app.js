0eb8e33ab96d00458deb96344e761095a94f9017
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-05 19:08:50 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+var character = 'adam';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(input => {
+    console.log(input);
+});
