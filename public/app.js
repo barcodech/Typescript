aec14e371b07cac55ff0a2635a43033000cbdc1c
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-05 20:00:24 +0700

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,19 +1,19 @@
 "use strict";
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} buy ${this.details} for ${this.amount}`;
-    }
-}
-const invoice1 = new Invoice('Adam', 'shirts', 250);
-const invoice2 = new Invoice('Jack', 'shoes', 300);
-let Invoices = [];
-Invoices.push(invoice1);
-Invoices.push(invoice2);
-Invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+const me = {
+    name: 'adam',
+    age: 50,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I have', amount);
+        return amount;
+    },
+};
+console.log(me);
+me.speak('morning');
+me.spend(500);
+const greeting = (person) => {
+    console.log('hello', person.name);
+};
+greeting(me);
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,28 @@
-class Invoice {
-
-  constructor(
-    
-  readonly client: string,
-  private details: string,
-  public amount: number
-
-  ){}
-
-  format() {
-    return `${this.client} buy ${this.details} for ${this.amount}`;
-  }
+interface Person {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
 }
 
-const invoice1 = new Invoice('Adam','shirts',250);
-const invoice2 = new Invoice('Jack','shoes',300);
+const me: Person = {
+  name: 'adam',
+  age: 50,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I have', amount);
+    return amount;
+  },
+};
 
-let Invoices: Invoice[] = [];
-Invoices.push(invoice1);
-Invoices.push(invoice2);
+console.log(me);
+me.speak('morning');
+me.spend(500);
 
+const greeting = (person: Person): void => {
+  console.log('hello',person.name);
+}
 
-Invoices.forEach(inv => {
-  console.log(inv.client,inv.amount,inv.format());
-})
+greeting(me);
