3c81323d05c8a03219b5c84b2fa4ea86b9e0b623
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-05 20:06:09 +0700

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+import { Formatter } from "./interfaces/Formatter.js";
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+ 
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+ 
+  let doc: Formatter;
+  if (type.value === 'invoice'){
+    doc = new Invoice(tofrom.value,details.value,amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value,details.value,amount.valueAsNumber);
+  }
+  list.render(doc,type.value,'end');
+});
