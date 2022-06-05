8f94f9eed05e1118fc0e3eb581f9c23969e57b6d
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-05 19:46:40 +0700

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,17 +1,7 @@
 "use strict";
-let hello = () => {
-    console.log('Hello');
+let simple = (name, word) => {
+    console.log(`${name} says ${word}`);
 };
-hello = () => {
-    console.log('Hello2');
+let simple2 = (Adam) => {
+    console.log(`${Adam.name} is ${Adam.age} years old`);
 };
-let add = (a, b, c = 50) => {
-    console.log(a + b);
-    console.log(c);
-};
-add(10, 20, 60);
-let plus = (a, b) => {
-    return a + b;
-};
-let result = plus(1, 2);
-console.log(result);
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,10 @@
-let hello = () => {
-  console.log('Hello');
+type Vartype1 = string|number;
+type Objtype1 = {name: string, age: number};
 
-}
-
-hello = () => {
-  console.log('Hello2');
-}
-
-let add = (a: number,b:number,c: number | string = 50): void => {
-  console.log(a+b);
-  console.log(c);
-}
-
-add(10,20,60);
-
-let plus = (a: number, b: number): number => {
-  return a+b;
-}
-
-let result = plus(1,2);
-console.log(result);
+let simple = (name: string, word: Vartype1) => {
+    console.log(`${name} says ${word}`);
+  }
+ 
+let simple2 = (Adam: Objtype1) => {
+    console.log(`${Adam.name} is ${Adam.age} years old`);
+  }
