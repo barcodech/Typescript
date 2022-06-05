db2fbd652e2994e3aaa8f95341820dbccc293df7
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-05 19:49:20 +0700

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,7 +1,19 @@
 "use strict";
-let simple = (name, word) => {
-    console.log(`${name} says ${word}`);
+//let hello: Function;
+let function1;
+function1 = (name, surname) => {
+    console.log(`${name} ${surname}`);
 };
-let simple2 = (Adam) => {
-    console.log(`${Adam.name} is ${Adam.age} years old`);
+let function2;
+function2 = (number1, number2, words) => {
+    if (words === 'hello') {
+        return number1 + number2;
+    }
+    else {
+        return number1 - number2;
+    }
+};
+let function3;
+function3 = (adam) => {
+    console.log(`${adam.name} is ${adam.age} years old`);
 };
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
-type Vartype1 = string|number;
-type Objtype1 = {name: string, age: number};
+//let hello: Function;
 
-let simple = (name: string, word: Vartype1) => {
-    console.log(`${name} says ${word}`);
+let function1: (a: string,b: string) => void;
+
+function1 = (name: string, surname: string) => {
+  console.log(`${name} ${surname}`);
+}
+
+let function2: (a: number,b: number, c: string) => number;
+
+function2 = (number1: number,number2: number, words: string) => {
+  if (words === 'hello'){
+    return number1 + number2;
+  } else {
+    return number1 - number2;
   }
- 
-let simple2 = (Adam: Objtype1) => {
-    console.log(`${Adam.name} is ${Adam.age} years old`);
-  }
+}
+
+type person = {name: string,age: number};
+
+let function3: (obj: person) => void;
+
+function3 = (adam: {name: string,age: number}) => {
+  console.log(`${adam.name} is ${adam.age} years old`);
+}
